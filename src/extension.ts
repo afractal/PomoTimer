bfbbb8847a30ff078e5a1b3268ded238b28ccace
hermesxgjini <hermesxgjini@gmail.com>
2017-03-20 23:53:35 +0100

    idle state init

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,38 @@
-'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { window, commands } from 'vscode';
+import { ExtensionContext, StatusBarAlignment } from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: ExtensionContext) {
+    let playStatusBarItem = window.createStatusBarItem(StatusBarAlignment.Right, 1);
+    playStatusBarItem.color = 'white';
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "pomotimer" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+    let displayTimerDisposable = commands.registerCommand('pomotimer.displayTimer', () => {
+        playStatusBarItem.text = '$(triangle-right)';
+        playStatusBarItem.command = 'pomotimer.startTimer';
+        playStatusBarItem.tooltip = 'Start timer';
+        playStatusBarItem.show();
     });
 
-    context.subscriptions.push(disposable);
+    let startTimerDisposable = commands.registerCommand('pomotimer.startTimer', () => {
+        setTimeout(() => {
+            const statusBarText = playStatusBarItem.text;
+            if (statusBarText.includes('time')) {
+                playStatusBarItem.text = playStatusBarItem.text +
+                    playStatusBarItem.text.substring(
+                        statusBarText.indexOf(':'),
+                        statusBarText.length);
+            }
+            playStatusBarItem.text = `$(primitive-square) time:${1}`;
+        }, 2000);
+        // do { }
+        // while (++counter < 10);
+    });
+
+
+    context.subscriptions.push(displayTimerDisposable, startTimerDisposable);
 }
 
-// this method is called when your extension is deactivated
+
+
 export function deactivate() {
 }
